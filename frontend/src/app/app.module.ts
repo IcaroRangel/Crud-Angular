f06bf445e5f99f6d1287802c9b10a5462831e9ce
Icaro <icarorangel05@gmail.com>
2022-01-20 16:35:20 -0300

    navigation product-create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,10 +13,11 @@ import { NavComponent } from "./components/template/nav/nav.component";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
+import { MatButtonModule } from "@angular/material/button";
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
-import { ForDirective } from './directives/for.directive';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ForDirective } from "./directives/for.directive";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
