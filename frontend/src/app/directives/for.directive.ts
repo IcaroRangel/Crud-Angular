a5571e3671dfc6ec85cf5e0bb34b1cee160fdc79
Icaro <icarorangel05@gmail.com>
2022-01-20 15:27:35 -0300

    directive-for

--- frontend/src/app/directives/for.directive.ts
+++ frontend/src/app/directives/for.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from "@angular/core";
+import {
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from "@angular/core";
 
 @Directive({
   selector: "[myFor]",
 })
-export class ForDirective {
-  constructor() {
-    console.log("MyFor");
+export class ForDirective implements OnInit {
+  @Input("myForEm") numbers!: number[];
+
+  constructor(
+    private container: ViewContainerRef,
+    private template: TemplateRef<any>
+  ) {}
+
+  ngOnInit(): void {
+    for (let number of this.numbers) {
+      this.container.createEmbeddedView(this.template, { $implicit: number });
+    }
+    console.log(this.numbers);
   }
 }
